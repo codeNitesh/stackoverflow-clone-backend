2e65c42b276794ebdb9f4995960f25135195d81e
Nitesh Soni <niteshmitrc@gmail.com>
2023-09-15 22:55:09 +0530

    #️⃣  USER PASSWORD HASHING ADDED

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -6,7 +7,7 @@ const userSchema = new mongoose.Schema({
     unique: true,
     required: true,
   },
-  // TODO: PASSWORD TO BE STORED AS HASH
+  // PASSWORD TO BE STORED AS HASH
   password: {
     type: String,
     required: true,
@@ -15,6 +16,18 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// PRE - Hashing the password before saving it
+userSchema.pre("save", function (next) {
+  const currentUser = this;
+  if (!currentUser.isModified("password")) return next();
+
+  bcrypt.hash(currentUser.password, 10, (err, hash) => {
+    if (err) return next(err);
+    currentUser.password = hash;
+    next();
+  });
 });
 
 module.exports = mongoose.model("User", userSchema);
